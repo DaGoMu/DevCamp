b4d400c2b0268dabb78ee8c645942bad0be415d7
DaGoMu <davidgmmo@gmail.com>
2023-02-01 12:16:35 +0100

    Initial commit
    Creación y reorganización de la carpeta DevCamp

---
+++ FullStack/Checkpoints/M2C3/M2C3.py
@@ -0,0 +1,54 @@
+# Ejercicio 1
+
+my_string = "Este es un string"
+my_number = 2023
+my_list = ['item1','item2', 'item3']
+my_boolean = True
+
+
+# Ejercicio 2
+
+string_ej_2 = my_string[0:3]
+#print(string_ej_2)
+
+
+# Ejercicio 3
+
+my_list_first = my_list[0]
+# print(my_list_first)
+
+
+# Ejercicio 4
+
+new_num = my_number + 10
+#print(new_num)
+
+
+# Ejercicio 5 
+
+last_my_list = my_list[-1]
+#print(last_my_list) 
+
+
+# Ejercicio 6 
+
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+#print(names_list)
+
+# Ejercicio 7
+my_string_first = my_string[0:4].upper()
+
+new_string = my_string.replace("Este", my_string_first)
+#print(new_string)
+
+
+# Ejercicio 8
+
+sentence = f"Mi numero es el {my_number}"
+#print(sentence)
+
+
+# Ejercicio 9 
+
+print('hello world')
